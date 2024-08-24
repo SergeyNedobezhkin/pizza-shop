adc96543c0e95dd132b0d6c2e851de9d9379408c
Sergey Nedobezhkin <zelenka1633@gmail.com>
2024-08-24 16:37:33 +0400

    Create TopBar and Rename shared components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import { Button } from "@/components/ui/button";
+import { Container, SortPopup, TopBar } from "@/components/shared";
+import { Title } from "@/components/shared/title";
 
 export default function Home() {
   return (
     <>
-      <h1>Главная</h1>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+
+      <TopBar />
+      <div style={{ height: "3000px" }} />
     </>
   );
 }
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,2 +1,4 @@
 export { Header } from "./header";
 export { Container } from "./container";
+export { SortPopup } from "./SortPopup";
+export { TopBar } from "./TopBar";
